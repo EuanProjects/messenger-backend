422c174bda37586637e1a85acfe710d80e6bfd32
ejcanoy <euan.james.canoy@gmail.com>
2024-06-05 16:31:45 -0700

    connected mongodb

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config()
+
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const mongoDB = process.env.MONGO_DB_URL;
+main().then(()=>console.log('connected')).catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');

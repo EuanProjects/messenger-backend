814f134391159bd7a983112482980d1cd069b389
ejcanoy <euan.james.canoy@gmail.com>
2024-07-10 14:17:44 -0700

    added create profile feature

--- controller/profile.js
+++ controller/profile.js
@@ -1,5 +1,39 @@
 const Profile = require("../models/profile");
 const asyncHandler = require("express-async-handler");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const profile = await Profile.findOne({ username: username });
+            if (!profile) {
+                return done(null, false, { message: "Incorrect username" });
+            };
+            const match = await bcrypt.compare(password, profile.password);
+            if (!match) {
+                return done(null, false, { message: "Incorrect password" })
+            }
+            return done(null, profile);
+        } catch (err) {
+            return done(err);
+        };
+    })
+);
+
+passport.serializeUser((profile, done) => {
+    done(null, profile.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const profile = await Profile.findById(id);
+        done(null, profile);
+    } catch (err) {
+        done(err);
+    };
+});
 
 
 // figure out which routes I need to restrict
@@ -11,17 +45,33 @@ exports.profileGet = asyncHandler(async (req, res, next) => {
 })
 
 exports.profilePost = asyncHandler(async (req, res, next) => {
-    // check if username exists first
-    
-    console.log(req.username, req.password);
-    const newProfile = new Profile({
-        username: req.body.username,
-        password: req.body.password,
-        friends: [],
-        setup: false,
-        request: [],
-    })
-    const savedProfile = await newProfile.save();
+    try {
+        const profile = await Profile.find({ username: req.body.username });
+        if (profile.length > 0) {
+            return res.status(400).send('Profile already exists');
+        }
 
-    res.send(savedProfile);
-})
+        bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+            if (err) {
+                return next(err); 
+            }
+
+            try {
+                const newProfile = new Profile({
+                    username: req.body.username,
+                    password: hashedPassword,
+                    friends: [],
+                    setup: false,
+                    request: [],
+                });
+                const savedProfile = await newProfile.save();
+                return res.send('Profile created successfully');
+            } catch (err) {
+                return next(err);
+            }
+        });
+    } catch (err) {
+        return next(err);
+    }
+});
+

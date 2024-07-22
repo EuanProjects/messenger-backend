a812f654e47f98e10fc83ca21f98644af414ceab
ejcanoy <euan.james.canoy@gmail.com>
2024-07-22 14:02:24 -0700

    Added profile firends and convos

--- controller/profile.js
+++ controller/profile.js
@@ -1,7 +1,13 @@
 const Profile = require("../models/profile");
+const Conversation = require("../models/conversation");
+const Message = require("../models/message");
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
 const auth = require("../middleware/auth");
+const mongoose = require("mongoose");
+const conversation = require("../models/conversation");
+require('dotenv').config()
+
 
 
 exports.profilesGet = asyncHandler(async (req, res, next) => {
@@ -42,21 +48,68 @@ exports.profilePost = asyncHandler(async (req, res, next) => {
             }
 
             try {
+                const session = await mongoose.startSession();
+                session.startTransaction();
+
+                const starterProfiles = process.env.STARTER_PROFILES.split(",");
+                console.log(starterProfiles);
+
                 const newProfile = new Profile({
                     username: req.body.username,
                     picture: "",
                     name: "",
                     password: hashedPassword,
-                    friends: [],
+                    friends: starterProfiles,
                     setup: false,
                 });
+
+                console.log(newProfile);
                 const savedProfile = await newProfile.save();
+
+                for (let i = 0; i < starterProfiles.length; i++) {
+                    await Profile.findByIdAndUpdate(starterProfiles[i], {
+                        $push: { friends: newProfile._id }
+                    });
+                }
+
+                const curDate = new Date()
+                const conversation1 = new Conversation({
+                    profileIds: [...starterProfiles, newProfile._id],
+                    theme: "Default",
+                    lastUpdated: curDate,
+                    lastMessage: null,
+                })
+                
+                await conversation1.save();
+
+                const conversation2 = new Conversation({
+                    profileIds: [starterProfiles[1], newProfile._id],
+                    theme: "Default",
+                    lastUpdated: curDate,
+                    lastMessage: null,
+                })
+                
+                await conversation2.save();
+
+                const message = new Message({
+                    timestamp: curDate,
+                    conversationId: conversation2._id,
+                    message: `Hello ${newProfile.username}!`,
+                    profileId: starterProfiles[1]
+                })
+
+                await message.save();
+
+                await session.commitTransaction();
+                session.endSession();
                 return res.send('Profile created successfully');
             } catch (err) {
                 return next(err);
             }
         });
     } catch (err) {
+        await session.abortTransaction();
+        session.endSession();
         return next(err);
     }
 });
